93df11aae372e9d04fc8b39e278f263efd4d2dd2
CoderSales <32943259+CoderSales@users.noreply.github.com>
2024-09-14 04:44:28 +0100

    error Add error output as comment See last commit
    for
    error explanation
    
    immutable
    variable
    demonstration

--- programmiz/attempt-01/main.rs
+++ programmiz/attempt-01/main.rs
@@ -7,3 +7,21 @@ fn main() {
     x = 2;
     println!("x = {}", x);
 }
+
+/*
+error[E0384]: cannot assign twice to immutable variable `x`
+ --> main.rs:7:5
+  |
+3 |     let x = 1;
+  |         -
+  |         |
+  |         first assignment to `x`
+  |         help: consider making this binding mutable: `mut x`
+...
+7 |     x = 2;
+  |     ^^^^^ cannot assign twice to immutable variable
+
+error: aborting due to 1 previous error
+
+For more information about this error, try `rustc --explain E0384`.
+*/
