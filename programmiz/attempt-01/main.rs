f2a1638bbbebe27efe363caeebb37a7e410352dd
CoderSales <32943259+CoderSales@users.noreply.github.com>
2024-09-14 05:16:04 +0100

    Fix main rs Overwrite Add Floating Point
    Data Type
    Example

--- programmiz/attempt-01/main.rs
+++ programmiz/attempt-01/main.rs
@@ -1,19 +1,10 @@
 fn main() {
-    let x: u32 = -200;
+    // f32 floating point type
+    let x: f32 = 3.1;
+
+    // f64 floating point type
+    let y: f64 = 45.0000031;
 
     println!("x = {}", x);
+    println!("y = {}", y);
 }
-
-/*
-error[E0600]: cannot apply unary operator `-` to type `u32`
- --> main.rs:2:18
-  |
-2 |     let x: u32 = -200;
-  |                  ^^^^ cannot apply unary operator `-`
-  |
-  = note: unsigned values cannot be negated
-
-error: aborting due to 1 previous error
-
-For more information about this error, try `rustc --explain E0600`.
-*/
