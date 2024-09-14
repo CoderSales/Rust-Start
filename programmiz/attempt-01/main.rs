40f39a94bdb8e0da4f5c9657d1bb5c7604f81c1c
CoderSales <32943259+CoderSales@users.noreply.github.com>
2024-09-14 04:36:12 +0100

    specify numbering for placeholders

--- programmiz/attempt-01/main.rs
+++ programmiz/attempt-01/main.rs
@@ -2,6 +2,6 @@ fn main() {
     let age = 31;
     let name = "Jack";
   
-    // print the variables using println!
-    println!("Name = {}, Age = {}", name, age);
+    // print the variable using println!
+    println!("Name = {0}, Age = {1}", name, age);
 }
