d4b4fd5da9c544925355f3c68b3e9aac9cc0aaf4
CoderSales <32943259+CoderSales@users.noreply.github.com>
2024-09-14 04:51:58 +0100

    error const variable cannot be reassigned
    Add error
    as
    a
    comment

--- programmiz/attempt-01/main.rs
+++ programmiz/attempt-01/main.rs
@@ -1,6 +1,23 @@
 fn main() {
-    // declare a float constant
-    const PI: f32 = 3.14;
+    // declare a constant
+    const PI:f32 = 3.14;
+    println!("Initial Value of PI: {}", PI);
 
-    println!("Value of PI = {}", PI);
+    // change value of PI
+    PI = 535.23;
+    println!("Update Value of PI: {}", PI);
 }
+
+/*
+error[E0070]: invalid left-hand side of assignment
+ --> main.rs:7:8
+  |
+7 |     PI = 535.23;
+  |     -- ^
+  |     |
+  |     cannot assign to this expression
+
+error: aborting due to 1 previous error
+
+For more information about this error, try `rustc --explain E0070`.
+*/
