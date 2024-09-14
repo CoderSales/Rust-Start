61f6b297c204f8b3ae9e473fae0f526347136571
CoderSales <32943259+CoderSales@users.noreply.github.com>
2024-09-14 05:29:25 +0100

    Store special characters in character variablein
    using
    char
    keyword

--- programmiz/attempt-01/main.rs
+++ programmiz/attempt-01/main.rs
@@ -1,6 +1,8 @@
 fn main() {
     // char type
     let character: char = 'z';
+    let special_character: char = '$';
 
     println!("character = {}", character);
+    println!("special_character = {}", special_character);
 }
