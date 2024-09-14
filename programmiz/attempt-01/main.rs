5242a6c1d95642aca30c1bb6dfc8dd74f6bdf708
CoderSales <32943259+CoderSales@users.noreply.github.com>
2024-09-14 05:04:13 +0100

    error cannot apply minus to unsigned integer u32

--- programmiz/attempt-01/main.rs
+++ programmiz/attempt-01/main.rs
@@ -1,6 +1,19 @@
 fn main() {
-    // Unsigned integer type
-    let x: u32 = 300;
+    let x: u32 = -200;
 
     println!("x = {}", x);
 }
+
+/*
+error[E0600]: cannot apply unary operator `-` to type `u32`
+ --> main.rs:2:18
+  |
+2 |     let x: u32 = -200;
+  |                  ^^^^ cannot apply unary operator `-`
+  |
+  = note: unsigned values cannot be negated
+
+error: aborting due to 1 previous error
+
+For more information about this error, try `rustc --explain E0600`.
+*/
