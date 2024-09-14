4d106dd3a57e58e297ad910fe98509416fd5fa4d
CoderSales <32943259+CoderSales@users.noreply.github.com>
2024-09-14 05:35:07 +0100

    Add note on variable storing a number as char

--- programmiz/attempt-01/main.rs
+++ programmiz/attempt-01/main.rs
@@ -2,7 +2,16 @@ fn main() {
     // char type
     let character: char = 'z';
     let special_character: char = '$';
+    let numeric_character: char = '5';
+
 
     println!("character = {}", character);
     println!("special_character = {}", special_character);
+    println!("numeric_character = {}", numeric_character);
+    println!("5 is char not integer");
+    println!("due to single quotes");
+    println!("used when storing variable");
+
+    // '5' is char not integer due to
+    // single quotes.
 }
