9adfa425bfdab86b250178c1c3b3c5e02287aa0a
CoderSales <32943259+CoderSales@users.noreply.github.com>
2024-09-14 04:47:32 +0100

    Fix Add mutable variables Use mut keyword

--- programmiz/attempt-01/main.rs
+++ programmiz/attempt-01/main.rs
@@ -1,27 +1,9 @@
 fn main() {
-    // declare a variable with value 1
-    let x = 1;
-    println!("x = {}", x);
+    // declare a mutable variable with value 1
+    let mut x = 1;
+    println!("Value of x = {}", x);
 
     // change the value of variable x
     x = 2;
-    println!("x = {}", x);
+    println!("Updated value of x = {}", x);
 }
-
-/*
-error[E0384]: cannot assign twice to immutable variable `x`
- --> main.rs:7:5
-  |
-3 |     let x = 1;
-  |         -
-  |         |
-  |         first assignment to `x`
-  |         help: consider making this binding mutable: `mut x`
-...
-7 |     x = 2;
-  |     ^^^^^ cannot assign twice to immutable variable
-
-error: aborting due to 1 previous error
-
-For more information about this error, try `rustc --explain E0384`.
-*/
