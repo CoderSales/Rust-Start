35e348ad90a2215d4ff10bab847b70f25888e343
CoderSales <32943259+CoderSales@users.noreply.github.com>
2024-09-14 04:49:09 +0100

    Add constant variable Use const keyword

--- programmiz/attempt-01/main.rs
+++ programmiz/attempt-01/main.rs
@@ -1,9 +1,6 @@
 fn main() {
-    // declare a mutable variable with value 1
-    let mut x = 1;
-    println!("Value of x = {}", x);
+    // declare a float constant
+    const PI: f32 = 3.14;
 
-    // change the value of variable x
-    x = 2;
-    println!("Updated value of x = {}", x);
+    println!("Value of PI = {}", PI);
 }
