5ccb85806bf84a000e90e731bf84ec789959e186
CoderSales <32943259+CoderSales@users.noreply.github.com>
2024-09-14 04:37:10 +0100

    print newline character

--- programmiz/attempt-01/main.rs
+++ programmiz/attempt-01/main.rs
@@ -1,7 +1,3 @@
 fn main() {
-    let age = 31;
-    let name = "Jack";
-  
-    // print the variables using println!
-    println!("Name = {name}, Age = {age}");
+    print!("Rust is fun!\nI love Rust programming.");
 }
