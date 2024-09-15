8b26fe895783a2ba7e898c77e23731731a8276b2
CoderSales <32943259+CoderSales@users.noreply.github.com>
2024-09-15 16:25:30 +0100

    Add break and continue example

--- programmiz/attempt-01/main.rs
+++ programmiz/attempt-01/main.rs
@@ -1,26 +1,20 @@
 fn main() {
-    let mut i = 1;
-    
-    // start of outer loop
-    while i <= 5 { //  Note1: 5 lines
-        let mut j = 1;
-            
-        // start of inner loop
-        while j <= 5 { // Note2: would be 5 asterisks
-            j += 1;
-            
-            // condition to skip iteration of the inner loop
-            if j == 3 { // Note3: but skip 3 so 4 asterisks
-                // move to the next iteration of the inner loop
-                continue;
-            }
-            
-            print!("*");
+    let mut number = 0;
+
+    loop {
+        number += 1;
+
+        // condition to skip the iteration
+        if number == 3 {
+            continue;
         }
         
-        println!("");
+        // condition to exit the loop
+        if number > 5 {
+            break;
+        }
         
-        i += 1;
+        println!("{}", number);
     }
 }
 
@@ -36,16 +30,14 @@ Section Title:
 Working of continue Keyword in Rust
 
 Subsection Title:
-Rust continue with Nested Loops
+break and continue with loop
 
 
 Output:
 
-****
-****
-****
-****
-****
-
+1
+2
+4
+5
 
 */
