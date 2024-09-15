34d4f33d444f1b03e7adb6d480403084c02365d4
CoderSales <32943259+CoderSales@users.noreply.github.com>
2024-09-15 14:50:44 +0100

    Add Character Type Example

--- programmiz/attempt-01/main.rs
+++ programmiz/attempt-01/main.rs
@@ -1,8 +1,6 @@
 fn main() {
-    // boolean type
-    let flag1: bool = true;
-    let flag2: bool = false;
+    // char type
+    let character: char = 'z';
 
-    println!("flag1 = {}", flag1);
-    println!("flag2 = {}", flag2);
+    println!("character = {}", character);
 }
