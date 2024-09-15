dac7c4064188cc4380199be1dac1cda0fe554640
CoderSales <32943259+CoderSales@users.noreply.github.com>
2024-09-15 15:45:05 +0100

    Add plus minus times example

--- programmiz/attempt-01/main.rs
+++ programmiz/attempt-01/main.rs
@@ -1,14 +1,24 @@
 fn main() {
-    let decimal: f32 = 65.321;
-  
-    // convert float to char data type
-    let character = decimal as char;
+    let a = 20;
+    let b = 2;
 
-    println!("decimal = {}", decimal);
-    println!("character = {}", character);
+    // add two variables using + operator
+    let x = a + b;
+    println!("{} + {} = {}", a, b, x);
+
+    // subtract two variables using - operator
+    let y = a - b;
+    println!("{} - {} = {}", a, b, y);
+
+    // multiply two variables using * operator
+    let z = a * b;
+    println!("{} * {} = {}", a, b, z);
 }
-/*
-Limitations of Type Casting
 
-error[E0604]: only `u8` can be cast as `char`, not `f32`
+/*
+Example 1: Addition, Subtraction and Multiplication Operators
+
+20 + 2 = 22
+20 - 2 = 18
+20 * 2 = 40
 */
