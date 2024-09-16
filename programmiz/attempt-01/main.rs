14518eaec05c51c14b5a33b51e9cd6fe93e9b36d
CoderSales <32943259+CoderSales@users.noreply.github.com>
2024-09-16 10:33:30 +0100

    Add array example without data type

--- programmiz/attempt-01/main.rs
+++ programmiz/attempt-01/main.rs
@@ -1,8 +1,8 @@
 fn main() {
-    // initialization of array with data type
-    let numbers: [i32; 5] = [1, 2, 3, 4, 5];
+    // initialization of array without data type
+    let numbers = [1, 2, 3, 4, 5];
     
-    println!("Array of numbers = {:?}", numbers);
+    println!("array of numbers = {:?}", numbers);
 }
 
 /*
@@ -17,12 +17,9 @@ Section Title:
 Creating an Array in Rust
 
 Subsection Title:
-Array with Data Type in Rust
+Array without Data Type in Rust
 
 Output:
-Array of numbers = [1, 2, 3, 4, 5]
-
-created array of numbers with:
-let numbers: [i32; 5] = [1, 2, 3, 4, 5];
+array of numbers = [1, 2, 3, 4, 5]
 
 */
