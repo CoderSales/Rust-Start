60e5efe3d5fe278a0f32e2814e04e2f635fac6b1
CoderSales <stephenobrien1@gmail.com>
2024-10-26 20:07:30 +0100

    Debug difference with tutorial Error in code

--- programiz/attempt-01/main.rs
+++ programiz/attempt-01/main.rs
@@ -3,13 +3,15 @@ use std::fs::File;
 fn main() {
     let data_result = File::open("data.txt");
 
-    // using match for Result type
-    let data_file = match data_result {
-        Ok(file) => file,
-        Err(error) => panic!("Problem opening the data file: {:?}", error),
-    };
+    print!("{}", data_result);
 
-    println!("Data file", data_file);
+    // // using match for Result type
+    // let data_file = match data_result {
+    //     Ok(file) => file,
+    //     Err(error) => panic!("Problem opening the data file: {:?}", error),
+    // };
+
+    // println!("Data file", data_file);
 }
 
 /*
