71c27b9a17203f4d439f54531d97584b57a94c5d
CoderSales <stephenobrien1@gmail.com>
2024-10-26 15:50:11 +0100

    Update heading in main rs comments

--- programiz/attempt-01/main.rs
+++ programiz/attempt-01/main.rs
@@ -1,15 +1,15 @@
 use std::collections::HashSet;
 
 fn main() {
-    let hashset1 = HashSet::from([2, 7, 8]);
-    let hashset2 = HashSet::from([1, 2, 7]);
+    let hashset1 = HashSet::from([1, 2, 3, 4]);
+    let hashset2 = HashSet::from([4, 3, 2]);
     
-    // Intersection of hashsets
-    let result: HashSet<_> = hashset1.intersection(&hashset2).collect();
+    // Difference between hashsets
+    let result: HashSet<_> = hashset1.difference(&hashset2).collect();
     
     println!("hashset1 = {:?}", hashset1);
     println!("hashset2 = {:?}", hashset2);
-    println!("intersection = {:?}", result);
+    println!("difference = {:?}", result);
 }
 
 /*
@@ -23,11 +23,11 @@ Section Title:
 Set Operations
 
 Subsection Title:
-2. Intersection of two Sets
+3. Difference between two Sets
 
 ### Documentation
 
-34-Tutorial-HashSet-onwards.md
+35-Tutorial-Hashset-continued-onwards.md
 
 Output:
 hashset1 = {2, 7, 8}
